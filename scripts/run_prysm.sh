004a6703c4ae92c7d1efad80aed4414e175be92d
mxmar <macht4tech@gmail.com>
2020-12-14 19:48:39 +0100

    prysm docker build
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- scripts/run_prysm.sh
+++ scripts/run_prysm.sh
@@ -42,9 +42,9 @@ bazel run //beacon-chain --define=ssz=$SPEC_VERSION -- \
   --verbosity=debug \
   --interop-eth1data-votes \
   --chain-config-file=$TESTNET_DIR/config.yaml \
-  --contract-deployment-block=0 \
   --deposit-contract=0x8A04d14125D0FDCDc742F4A05C051De07232EDa4 \
-  --interop-genesis-state=$TESTNET_DIR/genesis.ssz &
+  --interop-genesis-state=$TESTNET_DIR/genesis.ssz \
+  --accept-terms-of-use &
 
 sleep 5
 
@@ -52,5 +52,3 @@ bazel run //validator --define=ssz=$SPEC_VERSION -- \
   --chain-config-file=$TESTNET_DIR/config.yaml \
   --disable-accounts-v2=true \
   --verbosity=debug \
-  --keymanager=wallet \
-  --keymanageropts=$PRY_DATADIR/prysm/keymanager_opts.json
