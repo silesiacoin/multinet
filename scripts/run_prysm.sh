0f1239519a56054da318604878c1f7a720a7c299
mxmar <macht4tech@gmail.com>
2020-12-15 09:54:19 +0100

    prysm docker build v5
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- scripts/run_prysm.sh
+++ scripts/run_prysm.sh
@@ -39,19 +39,10 @@ bazel run //beacon-chain --define=ssz=$SPEC_VERSION -- \
   --pprof \
   --rpc-host=0.0.0.0 \
   --rpc-port=4000 \
-  --http-web3provider=http://34.78.227.45:8545/ \
+  --http-web3provider=https://mainnet.infura.io/v3/9e53d9a806764e0b9f2dcf5ccf9161a2 \
   --verbosity=debug \
   --interop-eth1data-votes \
   --chain-config-file=$TESTNET_DIR/config.yaml \
   --deposit-contract=0x8A04d14125D0FDCDc742F4A05C051De07232EDa4 \
   --interop-genesis-state=$TESTNET_DIR/genesis.ssz \
-  --accept-terms-of-use &
-
-sleep 5
-
-bazel run //validator --define=ssz=$SPEC_VERSION -- \
-  --chain-config-file=$TESTNET_DIR/config.yaml \
-  --disable-accounts-v2=true \
-  --verbosity=debug \
-  --accept-terms-of-use \
-  --wallet-dir=$PRY_DATADIR/prysm/wallets
+  --accept-terms-of-use
