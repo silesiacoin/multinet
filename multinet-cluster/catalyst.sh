6ecd8e81196d1e212fbab0662ef3c2a717f0666d
Patryk Krakos <patryk.krakos@m8b.pl>
2021-01-15 17:07:19 +0100

    feature/prysm-validator

--- multinet-cluster/catalyst.sh
+++ multinet-cluster/catalyst.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo catalyst;
+echo "$MULTINET_POD_NAME";
+
 FULLENODE=$(echo enode://$(cat /root/multinet/repo/data/$MULTINET_POD_NAME/enode.txt)@$MULTINET_POD_IP:30303);
 echo "waiters are starting";
 
@@ -13,3 +16,8 @@ fi
 while grep -q "teku-catalyst" /root/multinet/repo/data/common/enodes.txt; do
   sleep 5;
 done
+
+cp /root/multinet/repo/data/common/enodes.txt /root/multinet/repo/data/$MULTINET_POD_NAME/geth/static-nodes.json
+cp /root/multinet/repo/data/common/enodes.txt /root/multinet/repo/data/$MULTINET_POD_NAME/geth/trusted-nodes.json
+./geth --rpc --rpcapi net,eth,eth2 --etherbase 0x1000000000000000000000000000000000000000 --datadir /root/multinet/repo/data/$(MULTINET_POD_NAME) --rpccorsdomain "*" --rpcaddr "$MULTINET_POD_IP" --verbosity 5 --ethstats "$MULTINET_POD_NAME:$CATALYST_STATS_LOGIN_SECRET@$CATALYST_STATS_HOST:80"
+echo "Done";
--- multinet-cluster/genesis.sh
+++ multinet-cluster/genesis.sh
@@ -18,16 +18,17 @@ echo "$GENESIS_BODY" > /root/multinet/repo/data/common/genesis.json;
 ./bootnode -nodekeyhex $(cat /root/multinet/repo/data/$MULTINET_POD_NAME/geth/nodekey) -writeaddress > /root/multinet/repo/data/$MULTINET_POD_NAME/enode.txt;
 ./teku/bin/teku genesis mock --output-file /root/multinet/repo/data/common/genesis.ssz --eth1-block-hash $ETH_1_BLOCK_HASH --genesis-time $GENESIS_TIME --validator-count $ETH2_VALIDATORS;
 FULLENODE=$(echo enode://$(cat /root/multinet/repo/data/$MULTINET_POD_NAME/enode.txt)@$MULTINET_POD_IP:30303);
-echo "waiters are starting";
-
-while  [ ! -f /root/multinet/repo/data/common/enodes.txt ]; do
-  sleep 5;
-done
-
-if grep -q "teku-catalyst" /root/multinet/repo/data/common/enodes.txt; then
-  sed -i -e "s~$MULTINET_POD_NAME~$FULLENODE~g" /root/multinet/repo/data/common/enodes.txt
-fi
-
-while grep -q "teku-catalyst" /root/multinet/repo/data/common/enodes.txt; do
-  sleep 5;
-done
+#echo "waiters are starting";
+#
+#while  [ ! -f /root/multinet/repo/data/common/enodes.txt ]; do
+#  sleep 5;
+#done
+#
+#if grep -q "teku-catalyst" /root/multinet/repo/data/common/enodes.txt; then
+#  sed -i -e "s~$MULTINET_POD_NAME~$FULLENODE~g" /root/multinet/repo/data/common/enodes.txt
+#fi
+#
+#while grep -q "teku-catalyst" /root/multinet/repo/data/common/enodes.txt; do
+#  sleep 5;
+#done
+echo "Done";
