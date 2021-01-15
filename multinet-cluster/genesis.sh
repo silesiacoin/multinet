6d7c86dbbb824a85c22918492d1aeba13c7dc59a
Patryk Krakos <patryk.krakos@m8b.pl>
2021-01-15 17:26:44 +0100

    feature/prysm-validator

--- multinet-cluster/genesis.sh
+++ multinet-cluster/genesis.sh
@@ -16,6 +16,7 @@ fi
 echo "$GENESIS_BODY" > /root/multinet/repo/data/common/genesis.json;
 ./geth --datadir /root/multinet/repo/data/"$MULTINET_POD_NAME" init /root/multinet/repo/data/common/genesis.json;
 ./bootnode -nodekeyhex $(cat /root/multinet/repo/data/$MULTINET_POD_NAME/geth/nodekey) -writeaddress > /root/multinet/repo/data/$MULTINET_POD_NAME/enode.txt;
+echo $GENESIS_TIME
 ./teku/bin/teku genesis mock --output-file /root/multinet/repo/data/common/genesis.ssz --eth1-block-hash $ETH_1_BLOCK_HASH --genesis-time $GENESIS_TIME --validator-count $ETH2_VALIDATORS;
 FULLENODE=$(echo enode://$(cat /root/multinet/repo/data/$MULTINET_POD_NAME/enode.txt)@$MULTINET_POD_IP:30303);
 #echo "waiters are starting";
