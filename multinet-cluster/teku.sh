a782c04bc663abf26628dfcf99ab29644261f92f
blazejkrzak <blazej@m8b.pl>
2021-01-15 21:23:44 +0100

    Feature: Teku with catalyst as a pod - teku interop validators count  is correct, but it fails on rlp

--- multinet-cluster/teku.sh
+++ multinet-cluster/teku.sh
@@ -21,14 +21,12 @@ done
 
 echo "starting with Interop index $INTEROP_START_INDEX";
 
-./teku/bin/teku --Xinterop-enabled=true --Xinterop-owned-validator-count=$INTEROP_OWNED_VALIDATOR_COUNT --network=minimal --p2p-enabled=true --p2p-discovery-enabled=true --initial-state /root/multinet/repo/data/testnet/genesis.ssz --eth1-engine http://$MULTINET_POD_IP:8545 --rest-api-interface=$MULTINET_POD_IP --rest-api-host-allowlist="*" --rest-api-port=5051 --logging=all --rest-api-enabled=true --metrics-enabled=true --p2p-discovery-bootnodes=$TEKU_BOOTNODE_ENR --Xinterop-owned-validator-start-index $INTEROP_START_INDEX
-
-#./teku/bin/teku --Xinterop-enabled=true \
-#--Xinterop-owned-validator-count=$INTEROP_OWNED_VALIDATOR_COUNT \
-#--network=minimal --p2p-enabled=true --p2p-discovery-enabled=true \
-#--initial-state /root/multinet/repo/data/common/genesis.ssz \
-#--eth1-engine http://$MULTINET_POD_IP:8545 \
-#--rest-api-interface=$MULTINET_POD_IP --rest-api-host-allowlist="*" --rest-api-port=5051 \
-#--logging=all --rest-api-enabled=true --metrics-enabled=true \
-#--p2p-discovery-bootnodes=$TEKU_BOOTNODE_ENR \
-#--Xinterop-owned-validator-start-index $INTEROP_START_INDEX
+./teku/bin/teku --Xinterop-enabled=true \
+--Xinterop-owned-validator-count=$INTEROP_OWNED_VALIDATOR_COUNT \
+--network=minimal --p2p-enabled=true --p2p-discovery-enabled=true \
+--initial-state /root/multinet/repo/data/common/genesis.ssz \
+--eth1-engine http://$MULTINET_POD_IP:8545 \
+--rest-api-interface=$MULTINET_POD_IP --rest-api-host-allowlist="*" --rest-api-port=5051 \
+--logging=all --rest-api-enabled=true --metrics-enabled=true \
+--p2p-discovery-bootnodes=$TEKU_BOOTNODE_ENR \
+--Xinterop-owned-validator-start-index "$INTEROP_START_INDEX"
