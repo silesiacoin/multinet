9ce9a76f5854daab35dd3773cae5863c8efe58a0
Patryk Krakos <patryk.krakos@m8b.pl>
2021-01-20 14:18:05 +0100

    Increased Validator count to 16

--- multinet-cluster/catalyst.sh
+++ multinet-cluster/catalyst.sh
@@ -17,7 +17,9 @@ while grep -q "teku-catalyst" /root/multinet/repo/data/common/enodes.txt; do
   sleep 5;
 done
 
+cp /root/multinet/repo/data/common/UTC--2021-01-18T17-33-56.364337282Z--3a98976b3a0261038aac8dee8d47a225e2f831c3 /root/multinet/repo/data/$MULTINET_POD_NAME/keystore/
+
 cp /root/multinet/repo/data/common/enodes.txt /root/multinet/repo/data/$MULTINET_POD_NAME/geth/static-nodes.json
 cp /root/multinet/repo/data/common/enodes.txt /root/multinet/repo/data/$MULTINET_POD_NAME/geth/trusted-nodes.json
-./geth --rpc --rpcapi net,eth,eth2 --etherbase 0x1000000000000000000000000000000000000000 --datadir /root/multinet/repo/data/$MULTINET_POD_NAME --rpccorsdomain "*" --rpcaddr "$MULTINET_POD_IP" --verbosity 5 --ethstats "$MULTINET_POD_NAME:$CATALYST_STATS_LOGIN_SECRET@$CATALYST_STATS_HOST:80"
+./geth --rpc --rpcapi net,eth,eth2,web3,personal,admin,db,debug,miner,shh,txpool --etherbase 0x1000000000000000000000000000000000000000 --datadir /root/multinet/repo/data/$MULTINET_POD_NAME --rpccorsdomain "*" --rpcaddr "$MULTINET_POD_IP" --verbosity 5 --ethstats "$MULTINET_POD_NAME:$CATALYST_STATS_LOGIN_SECRET@$CATALYST_STATS_HOST:80" --unlock 0 --password "/root/multinet/repo/data/common/node.pwds" --allow-insecure-unlock --txpool.processtxs
 echo "Done";
